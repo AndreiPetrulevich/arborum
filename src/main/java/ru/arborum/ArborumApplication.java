52a4b414ab6b9208362dcd451d035960ec424ae3
Ond Re <aapetrulevich@icloud.com>
2022-05-24 12:12:14 +0300

    initial commit

---
+++ src/main/java/ru/arborum/ArborumApplication.java
@@ -0,0 +1,13 @@
+package ru.arborum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArborumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArborumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/arborum/ArborumApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.arborum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArborumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
